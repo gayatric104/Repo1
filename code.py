9208aa56fafd74d714018187303dbac35b954111
gayatri104 <gayatric104@gmail.com>
2021-02-16 16:33:04 +0530

    Revert "added changes to the files | code.py and index.py"
    This reverts commit 9d8e774f3b09f1c705dd5011fd737f40b08be218.

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import random
 # prints a random value from the list
 list1 = [1, 2, 3, 4, 5, 6]
 print(random.choice(list1))
-# dfjlaksjflkdsaj
+
 # prints a random item from the string
 string = "striver"
 print(random.choice(string))
