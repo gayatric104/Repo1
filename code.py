59b7498000e60aff641c16e813dde551e7d9515f
gayatri104 <gayatric104@gmail.com>
2021-02-18 12:01:02 +0530

    changes added in code.py and readme.txt

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 # import random
 import random as r
 import re
+import os
 # prints a random value from the list
 list1 = [1, 2, 3, 4, 5, 6]
 print(r.choice(list1))
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-this is repo project 
+this is repo project
